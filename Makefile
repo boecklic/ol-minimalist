220d7ea06299435f5d4fcef4b9c615cdeacb2ecd
Christoph Boecklin <christoph.boecklin@swisstopo.ch>
2019-05-08 11:47:45 +0200

    wip

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 
 .PHONY: setup
+setup:
 	python3 -m venv venv
 	source venv/bin/activate
 	pip install --upgrade pip
 	pip install -r requirements.txt
+	git submodule init
 
 
 .PHONY: update
@@ -25,8 +27,9 @@ run:
 
 .PHONY: deploy
 deploy:
-	gzip -c app.min.js | aws s3 --profile=aws-admin cp --content-type 'application/javascript' --content-encoding gzip --cache-control 'public, must-revalidate, proxy-revalidate, max-age=0' - s3://mf-geoadmin4-int-dublin/ol-minimalist/app.min.js
-	aws s3 --profile=aws-admin cp  --cache-control 'public, must-revalidate, proxy-revalidate, max-age=0' index.html s3://mf-geoadmin4-int-dublin/ol-minimalist/
+	gzip -c dist/app.min.js | aws s3 --profile=aws-admin cp --content-type 'application/javascript' --content-encoding gzip --cache-control 'public, must-revalidate, proxy-revalidate, max-age=0' - s3://mf-geoadmin4-int-dublin/ol-minimalist/app.min.js
+	aws s3 --profile=aws-admin cp  --cache-control 'public, must-revalidate, proxy-revalidate, max-age=0' dist/index.html s3://mf-geoadmin4-int-dublin/ol-minimalist/
+
 
 .PHONY: deploystyle
 deploystyle:
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,40 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');  // cleanup of the dist folder before a build
 const ClosurePlugin = require('closure-webpack-plugin');
 
 module.exports = {
-    entry: [
-        path.join(__dirname, 'src', 'main.js')
-    ],
-    output: {
-        path: __dirname,
-        filename: 'app.min.js'
+    entry: {
+        app: path.join(__dirname, 'src', 'index.js')
     },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].min.js' // this becomes e.g. app.min.js
+    },
+    plugins: [
+        new CleanWebpackPlugin(),    // do some cleanup first
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+            templateParameters: {
+                "target": "desktop"
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: 'mobile.html',
+            templateParameters: {
+                "target": "mobile"
+            }
+        })
+    ],
+    watchOptions: {
+        // watchOptions are necessary to track changes in 
+        // dependency modules (e.g. node_modules) as well
+        aggregateTimeout: 300,
+        poll: 1000, // How often check for changes (in milliseconds)
+    },/*,
     optimization: {
         minimizer: [
           new ClosurePlugin({mode: 'STANDARD'}, {
@@ -22,5 +47,5 @@ module.exports = {
             // renaming: false
           })
         ]
-    }
+    }*/
 };
